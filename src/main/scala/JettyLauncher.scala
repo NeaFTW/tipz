87833a283803d97304019b8eb1ac742c90059907
Nea <zheng_v@epita.fr>
2016-12-17 19:11:01 +0100

    [STANDLONE DEPLOY][ADD] Adding a jetty launch

---
+++ src/main/scala/JettyLauncher.scala
@@ -0,0 +1,25 @@
+/**
+  * Created by root on 17/12/16.
+  */
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
+import org.eclipse.jetty.webapp.WebAppContext
+import org.scalatra.servlet.ScalatraListener
+
+object JettyLauncher {
+  def main(args: Array[String]) {
+    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 80
+
+    val server = new Server(port)
+    val context = new WebAppContext()
+    context setContextPath "/"
+    context.setResourceBase("src/main/webapp")
+    context.addEventListener(new ScalatraListener)
+    context.addServlet(classOf[DefaultServlet], "/")
+
+    server.setHandler(context)
+
+    server.start
+    server.join
+  }
+}
